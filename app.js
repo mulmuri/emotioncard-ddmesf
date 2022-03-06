c5a352049480b98daca5d1396790701f5c2280b4
mulmuri <68294539+mulmuri@users.noreply.github.com>
2022-03-06 20:00:36 +0000

    update header bar

--- app.js
+++ app.js
@@ -30,11 +30,12 @@ Util = {
 
 class Room {
 
-    roomId = undefined;
+    roomId;
 
     view = {
         emotionList: [null, "감동적인", "감사한", "기대되는", "기쁜", "놀라운", "든든한", "만족스러운", "사랑스러운", "신나는", "열중한", "자랑스러운", "자신 있는", "재미 있는", "편안한", "평화로운", "홀가분한", "활기찬", "황홀한", "걱정스러운", "긴장한", "깜짝 놀란", "당황한", "두려운", "무서운", "불안한", "혼란스러운", "시무룩한", "웃픈", "답답한", "미운", "분한", "억울한", "짜증나는", "귀찮은", "무관심한", "부끄러운", "부러운", "싸늘한", "지루한", "피곤한", "괴로운", "그리운", "막막한", "미안한", "서운한", "슬픈", "실망스러운", "안타까운", "외로운", "우울한", "좌절한", "후회스러운", "궁금한", "위축되는", "서러운"],
-        cardColor: []
+        cardColor: [],
+        title: ""
     };
 
     card = {
@@ -71,8 +72,9 @@ class Room {
     }
 
 
-    constructor(roomId) {
+    constructor(roomId, title) {
         this.roomId = roomId;
+        this.view.title = title;
 
         for (var i=1; i<=55; i++) {
             var R = Math.floor(Math.random() * 8 + 8).toString(16) + Math.floor(Math.random() * 16).toString(16);
@@ -156,7 +158,7 @@ class Room {
 let room = new Map();
 
 app.post('/syncRequest', function(req, res) {
-    room.get(req.body.roomId).syncCallback(req, res, req.body.lastTime, req.body.roomId, 30);
+    room.get(req.body.roomId).syncCallback(req, res, req.body.lastTime, req.body.roomId, 250);
 });
 
 app.post('/selectCardRequest', function(req, res) {
@@ -182,9 +184,10 @@ app.post('/cardResetRequest', function(req, res) {
 
 app.post('/makeRoom', function(req, res) {
     var roomId = Util.generateRandomNumber(6);
+    var title = req.body.title;
     while (room.get(roomId) === null) roomId = Util.generateRandomNumber(6);
 
-    room.set(roomId, new Room(roomId));
+    room.set(roomId, new Room(roomId, title));
     var sid = room.get(roomId).user.addUser(req.body.name);
 
     res.redirect(`room?id=${roomId}&sid=${sid}`);
@@ -192,6 +195,9 @@ app.post('/makeRoom', function(req, res) {
 
 app.post('/enterRoom', function(req, res) {
     var roomId = req.body.roomId;
+    if (room.get(roomId) === undefined) {
+        res.redirect('main');
+    }
 
     var sid = room.get(roomId).user.addUser(req.body.name);
 
@@ -238,4 +244,4 @@ app.get('/room', function(req, res) {
 
 
 
-app.listen(3001, () => console.log('server is running'));
+app.listen(3000, () => console.log('server is running'));
